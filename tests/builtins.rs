0b6a6841426814ae346768b2389fc1c32a1e101f
Aleksander Heintz <alxandr@alxandr.me>
2023-08-06 00:58:40 +0200

    Improve For component

--- tests/builtins.rs
+++ tests/builtins.rs
@@ -21,22 +21,8 @@ fn for_iter() {
 		"<ul><li id=\"a\">a</li><li id=\"b\">b</li><li id=\"c\">c</li></ul>",
 		<ul>
 			<For items={items}>
-				{|f: &mut HtmlFormatter, item| write_html!(f, <li id=item>{item}</li>)}
+				{|f, item| write_html!(f, <li id=item>{item}</li>)}
 			</For>
 		</ul>
 	);
-}
-
-#[test]
-fn playground() {
-	let mut buffer = BytesMut::new();
-	let mut formatter = HtmlFormatter::new(&mut buffer);
-
-	let f_ref = &mut formatter;
-	write_stuffs(f_ref).unwrap();
-	write_stuffs(formatter).unwrap();
-}
-
-fn write_stuffs<'a>(mut f: impl AsMut<HtmlFormatter<'a>>) -> std::fmt::Result {
-	f.as_mut().write_content("foo")
 }
