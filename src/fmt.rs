b0acb926d789585596e68a8f57e4b611562e31e4
Aleksander Heintz <alxandr@alxandr.me>
2023-08-06 23:46:24 +0200

    docs: more docs

--- src/fmt.rs
+++ src/fmt.rs
@@ -26,6 +26,14 @@ impl<'a> HtmlAttributeFormatter<'a> {
 		}
 	}
 
+	/// Write raw bytes to the attribute formatter. Note that this method does no sanitization or escaping of
+	/// the values. If you want the values to be sanitized, use the [write] method insted.
+	///
+	/// # Arguments
+	///
+	/// - `raw`: A reference to the raw byte slice that will be written to the buffer.
+	///
+	/// [write]: Self::write
 	pub fn write_bytes(&mut self, raw: &[u8]) {
 		self.buffer.reserve(raw.len() + 3);
 		if !self.any_written {
@@ -36,12 +44,33 @@ impl<'a> HtmlAttributeFormatter<'a> {
 		self.buffer.put_slice(raw);
 	}
 
+	/// Writes escaped bytes to the attribute formatter, ensuring valid HTML attribute characters.
+	///
+	/// This method accepts a reference to a byte slice containing the content to be written to the
+	/// formatter's buffer. The provided `value` is escaped to ensure that it only contains valid HTML
+	/// attribute characters, preventing any unintentional issues with attribute values. The escaped
+	/// content is then written to the formatter's buffer.
+	///
+	/// # Arguments
+	///
+	/// - `value`: A reference to the raw byte slice containing the content to be escaped and written.
 	pub fn write(&mut self, value: &[u8]) {
 		self.write_bytes(&escape::attribute(value))
 	}
 
+	/// Reserves space in the buffer for writing additional bytes without re-allocation.
+	///
+	/// This method ensures that enough space is reserved in the formatter's buffer to accommodate
+	/// the writing of `additional` bytes without needing to reallocate memory. It's useful to call
+	/// this method before writing a significant amount of content, as it can help prevent frequent
+	/// reallocations and improve performance.
+	///
+	/// # Arguments
+	///
+	/// - `additional`: The number of additional bytes to reserve space for in the buffer.
 	pub fn reserve(&mut self, additional: usize) {
-		self.buffer.reserve(additional);
+		// add 3 for the opening and closing constant parts
+		self.buffer.reserve(additional + 3);
 	}
 
 	fn write_value(buffer: &mut BytesMut, value: impl HtmlAttributeValue) -> fmt::Result {
@@ -57,6 +86,14 @@ impl<'a> HtmlAttributeFormatter<'a> {
 	}
 }
 
+/// A formatter for serializing HTML nodes and content.
+///
+/// The `HtmlFormatter` struct provides a versatile way to serialize HTML nodes and content,
+/// ensuring proper spacing, indentation, and formatting. It's designed to handle various
+/// types of HTML content and produce well-structured and readable HTML output.
+///
+/// NOTE: Currently, no indentation/readibility is supported. The plan is to implement that
+/// later.
 pub struct HtmlFormatter<'a> {
 	buffer: &'a mut BytesMut,
 }
@@ -68,18 +105,55 @@ impl<'a> AsMut<HtmlFormatter<'a>> for HtmlFormatter<'a> {
 }
 
 impl<'a> HtmlFormatter<'a> {
+	/// Creates a new `HtmlFormatter` instance with the provided buffer.
+	///
+	/// # Arguments
+	///
+	/// - `buffer`: A mutable reference to the [BytesMut] buffer where the formatted content will be written.
+	///
+	/// # Returns
+	///
+	/// A new `HtmlFormatter` instance associated with the provided buffer.
 	pub fn new(buffer: &'a mut BytesMut) -> Self {
 		Self { buffer }
 	}
 
+	/// Writes raw bytes to the formatter's buffer without escaping.
+	///
+	/// This method appends the specified raw bytes to the formatter's buffer without performing any
+	/// additional escaping or modification. It provides a low-level, raw API for directly writing
+	/// content to the buffer, which can be useful for situations where the content is already
+	/// properly formatted and safe to include as-is.
+	///
+	/// # Arguments
+	///
+	/// - `raw`: A reference to the raw byte slice that will be written to the buffer.
 	pub fn write_bytes(&mut self, raw: &[u8]) {
 		self.buffer.extend_from_slice(raw);
 	}
 
+	/// Writes escaped bytes to the formatter's buffer, ensuring valid HTML characters.
+	///
+	/// This method accepts a reference to a byte slice containing the content to be written to
+	/// the formatter's buffer. The provided `value` is escaped to ensure that it only contains
+	/// valid HTML characters, preventing any unintentional issues with the produced HTML content.
+	///
+	/// # Arguments
+	///
+	/// - `value`: A reference to the raw byte slice containing the content to be escaped and written.
 	pub fn write(&mut self, value: &[u8]) {
 		self.write_bytes(&escape::text(value))
 	}
 
+	// Writes a DOCTYPE declaration to the formatter's buffer.
+	///
+	/// This method appends a DOCTYPE declaration to the formatter's buffer. The provided `value` is
+	/// escaped to ensure that it only contains valid characters for a DOCTYPE declaration. The resulting
+	/// DOCTYPE declaration is properly formatted and follows the standard syntax of "<!DOCTYPE ...>".
+	///
+	/// # Arguments
+	///
+	/// - `value`: A reference to the raw byte slice containing the content for the DOCTYPE declaration.
 	pub fn write_doctype(&mut self, value: &[u8]) {
 		const DOCTYPE_PREFIX: &[u8] = b"<!DOCTYPE ";
 		const DOCTYPE_SUFFIX: &[u8] = b">";
@@ -94,30 +168,80 @@ impl<'a> HtmlFormatter<'a> {
 		self.write_bytes(DOCTYPE_SUFFIX);
 	}
 
+	/// Writes the start of an opening HTML tag to the formatter's buffer.
+	///
+	/// This method appends the start of an opening HTML tag to the formatter's buffer. The provided `tag`
+	/// is used as the tag name, and the tag is not closed. is commonly followed by either [write_attribute_name],
+	/// [write_self_close_tag], or [write_open_tag_end].
+	///
+	/// # Arguments
+	///
+	/// - `tag`: A reference to the raw byte slice containing the tag name for the opening tag.
+	///
+	/// [write_attribute_name]: Self::write_attribute_name
+	/// [write_self_close_tag]: Self::write_self_close_tag
+	/// [write_open_tag_end]: Self::write_open_tag_end
 	pub fn write_open_tag_start(&mut self, tag: &[u8]) {
 		self.buffer.reserve(tag.len() + 1);
 		self.write_bytes(b"<");
 		self.write_bytes(tag);
 	}
 
+	/// Writes an HTML attribute name to the formatter's buffer.
+	///
+	/// This method appends an HTML attribute name to the formatter's buffer. The provided `name` is
+	/// used as the attribute name.
+	///
+	/// # Arguments
+	///
+	/// - `name`: A reference to the raw byte slice containing the attribute name.
 	pub fn write_attribute_name(&mut self, name: &[u8]) {
 		self.buffer.reserve(name.len() + 1);
 		self.write_bytes(b" ");
 		self.write_bytes(name);
 	}
 
+	/// Writes an HTML attribute value to the formatter's buffer.
+	///
+	/// This method appends an HTML attribute value to the formatter's buffer. The provided `value` is
+	/// an instance of a type implementing the [HtmlAttributeValue] trait. The value is written to the
+	/// buffer, ensuring proper formatting and escaping if required.
+	///
+	/// # Arguments
+	///
+	/// - `value`: An instance implementing the [HtmlAttributeValue] trait, representing the attribute value.
+	///
+	/// # Returns
+	///
+	/// A [std::fmt::Result] indicating the success or failure of the writing operation.
 	pub fn write_attribute_value(&mut self, value: impl HtmlAttributeValue) -> fmt::Result {
 		HtmlAttributeFormatter::write_value(self.buffer, value)
 	}
 
+	/// Writes a self-closing indicator to the formatter's buffer.
+	///
+	/// This method appends a self-closing indicator " />" to the formatter's buffer. It's commonly used
+	/// after writing an opening tag to indicate that the tag is self-closing and has no associated content.
 	pub fn write_self_close_tag(&mut self) {
 		self.write_bytes(b" />");
 	}
 
+	/// Writes the end of an opening HTML tag to the formatter's buffer.
+	///
+	/// This method appends the end of an opening HTML tag ">" to the formatter's buffer. It's commonly
+	/// used after writing the tag name and its attributes to indicate the completion of the tag's opening.
 	pub fn write_open_tag_end(&mut self) {
 		self.write_bytes(b">");
 	}
 
+	/// Writes an HTML end tag to the formatter's buffer.
+	///
+	/// This method appends an HTML end tag "</tag>" to the formatter's buffer. The provided `tag` is used
+	/// as the tag name for the end tag.
+	///
+	/// # Arguments
+	///
+	/// - `tag`: A reference to the raw byte slice containing the tag name for the end tag.
 	pub fn write_end_tag(&mut self, tag: &[u8]) {
 		self.buffer.reserve(tag.len() + 3);
 		self.write_bytes(b"</");
@@ -125,10 +249,31 @@ impl<'a> HtmlFormatter<'a> {
 		self.write_bytes(b">");
 	}
 
+	/// Writes HTML content to the formatter's buffer.
+	///
+	/// This method appends HTML content to the formatter's buffer. The provided `content` is an
+	/// instance of a type implementing the [HtmlContent] trait. The content is formatted and written
+	/// to the buffer according to the implementation of the [HtmlContent] trait.
+	///
+	/// # Arguments
+	///
+	/// - `content`: An instance implementing the [HtmlContent] trait, representing the HTML content to write.
+	///
+	/// # Returns
+	///
+	/// A [std::fmt::Result] indicating the success or failure of the writing operation.
 	pub fn write_content(&mut self, content: impl HtmlContent) -> fmt::Result {
 		content.fmt(self)
 	}
 
+	/// Writes an HTML comment to the formatter's buffer.
+	///
+	/// This method appends an HTML comment to the formatter's buffer. The provided `comment` is escaped
+	/// to ensure that it only contains valid characters for an HTML comment.
+	///
+	/// # Arguments
+	///
+	/// - `comment`: A reference to the raw byte slice containing the content for the HTML comment.
 	pub fn write_comment(&mut self, comment: &[u8]) {
 		const COMMENT_PREFIX: &[u8] = b"<!--";
 		const COMMENT_SUFFIX: &[u8] = b"-->";
@@ -143,6 +288,16 @@ impl<'a> HtmlFormatter<'a> {
 		self.write_bytes(COMMENT_SUFFIX);
 	}
 
+	/// Reserves space in the buffer for writing additional bytes without reallocation.
+	///
+	/// This method ensures that enough space is reserved in the formatter's buffer to accommodate
+	/// the writing of `additional` bytes without needing to reallocate memory. It's useful to call
+	/// this method before writing a significant amount of content, as it can help prevent frequent
+	/// reallocations and improve performance.
+	///
+	/// # Arguments
+	///
+	/// - `additional`: The number of additional bytes to reserve space for in the buffer.
 	pub fn reserve(&mut self, additional: usize) {
 		self.buffer.reserve(additional);
 	}
