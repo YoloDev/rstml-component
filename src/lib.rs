427a758bb7fc900f7f59fd5c1fc8e241fda45e16
Aleksander Heintz <alxandr@alxandr.me>
2023-08-18 11:36:11 +0200

    feat: export HtmlAttributes and formatter

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,10 @@ mod sanitize;
 
 pub use builtins::For;
 pub use component::HtmlComponent;
-pub use fmt::{HtmlAttributeFormatter, HtmlAttributeValue, HtmlContent, HtmlFormatter, RawText};
+pub use fmt::{
+	HtmlAttributeFormatter, HtmlAttributeValue, HtmlAttributes, HtmlAttributesFormatter, HtmlContent,
+	HtmlFormatter, RawText,
+};
 pub use rstml_component_macro::{html, move_html, write_html, HtmlComponent};
 
 #[cfg(feature = "sanitize")]
