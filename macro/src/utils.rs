b0d8c34a580b0f5ec47465a13ec50fec4ffad091
davidon-top <davidkozakke@gmail.com>
2023-10-21 15:13:33 +0200

    fix(utils): run cargo fmt

--- macro/src/utils.rs
+++ macro/src/utils.rs
@@ -1,7 +1,13 @@
-use proc_macro2::{TokenStream, Span};
-use quote::{quote, ToTokens};
-use syn::{parse::Parse, Ident, Token, punctuated::Punctuated, FnArg, Field, GenericParam, token::{Comma, Brace, Bracket, Paren}, TypeParam, Type, Generics, PatIdent, TypeImplTrait, Visibility, TypeTuple, ReturnType};
+use proc_macro2::{Span, TokenStream};
 use proc_macro2_diagnostics::Diagnostic;
+use quote::{quote, ToTokens};
+use syn::{
+	parse::Parse,
+	punctuated::Punctuated,
+	token::{Brace, Bracket, Comma, Paren},
+	Field, FnArg, GenericParam, Generics, Ident, PatIdent, ReturnType, Token, Type, TypeImplTrait,
+	TypeParam, TypeTuple, Visibility,
+};
 
 struct AttrParser {
 	vis: syn::Visibility,
@@ -17,9 +23,19 @@ impl Parse for AttrParser {
 	}
 }
 
-fn create_not_used_ident(used: Punctuated<GenericParam, Comma>, ident_suggestion: Option<Ident>) -> Ident {
+fn create_not_used_ident(
+	used: Punctuated<GenericParam, Comma>,
+	ident_suggestion: Option<Ident>,
+) -> Ident {
 	let mut ident = ident_suggestion.unwrap_or_else(|| Ident::new("V", Span::call_site()));
-	while used.iter().any(|used| {if let GenericParam::Type(type_param) = used {type_param.ident == ident} else {false}}/* used.ident == Some(ident.clone()) */) {
+	while used.iter().any(|used| {
+		if let GenericParam::Type(type_param) = used {
+			type_param.ident == ident
+		} else {
+			false
+		}
+	} /* used.ident == Some(ident.clone()) */)
+	{
 		ident = Ident::new(&format!("{}T", ident), Span::call_site());
 	}
 	ident
@@ -62,8 +78,19 @@ fn surround_type(ty: &mut Type, surround: impl Surround) {
 	*ty = Type::Verbatim(surround.trait_surround(type_tokens))
 }
 
-fn generate_generic(generics: Generics, pat_ident: PatIdent, impl_type: TypeImplTrait, vis: Visibility) -> (Field, TypeParam) {
-	let type_ident = create_not_used_ident(generics.params.clone(), Some(Ident::new(pat_ident.ident.to_string().to_uppercase().as_str(), Span::call_site())));
+fn generate_generic(
+	generics: Generics,
+	pat_ident: PatIdent,
+	impl_type: TypeImplTrait,
+	vis: Visibility,
+) -> (Field, TypeParam) {
+	let type_ident = create_not_used_ident(
+		generics.params.clone(),
+		Some(Ident::new(
+			pat_ident.ident.to_string().to_uppercase().as_str(),
+			Span::call_site(),
+		)),
+	);
 	let type_param = TypeParam {
 		attrs: vec![],
 		ident: type_ident.clone(),
@@ -97,10 +124,11 @@ fn generate_normal_field(pat_ident: PatIdent, vis: Visibility, ty: Type) -> Fiel
 fn generate_one(ty: Type, generics: &mut Generics, pat_ident: PatIdent, vis: Visibility) -> Field {
 	match ty {
 		Type::ImplTrait(impl_type) => {
-			let (field, type_param) = generate_generic(generics.clone(), pat_ident.clone(), impl_type, vis);
+			let (field, type_param) =
+				generate_generic(generics.clone(), pat_ident.clone(), impl_type, vis);
 			generics.params.push(GenericParam::Type(type_param));
 			field
-		},
+		}
 		Type::Array(array_type) => {
 			let new_ty = *array_type.elem;
 			let len = array_type.len;
@@ -108,13 +136,13 @@ fn generate_one(ty: Type, generics: &mut Generics, pat_ident: PatIdent, vis: Vis
 			suffix_type(&mut field.ty, quote!(; #len));
 			surround_type(&mut field.ty, array_type.bracket_token);
 			field
-		},
+		}
 		Type::Paren(paren_type) => {
 			let new_ty = *paren_type.elem;
 			let mut field = generate_one(new_ty, generics, pat_ident, vis);
 			surround_type(&mut field.ty, paren_type.paren_token);
 			field
-		},
+		}
 		Type::Ptr(ptr_type) => {
 			let new_ty = *ptr_type.elem;
 			let mut prefix_tokens = ptr_type.star_token.into_token_stream();
@@ -127,7 +155,7 @@ fn generate_one(ty: Type, generics: &mut Generics, pat_ident: PatIdent, vis: Vis
 			let mut field = generate_one(new_ty, generics, pat_ident, vis);
 			prefix_type(&mut field.ty, prefix_tokens);
 			field
-		},
+		}
 		Type::Reference(ref_type) => {
 			let new_ty = *ref_type.elem;
 			let mut prefix_tokens = ref_type.and_token.into_token_stream();
@@ -140,15 +168,15 @@ fn generate_one(ty: Type, generics: &mut Generics, pat_ident: PatIdent, vis: Vis
 			let mut field = generate_one(new_ty, generics, pat_ident, vis);
 			prefix_type(&mut field.ty, prefix_tokens);
 			field
-		},
+		}
 		Type::Slice(slice_type) => {
 			let new_ty = *slice_type.elem;
 			let mut field = generate_one(new_ty, generics, pat_ident, vis);
 			surround_type(&mut field.ty, slice_type.bracket_token);
 			field
-		},
+		}
 		Type::Tuple(typle_type) => {
-			let TypeTuple {paren_token, elems} = typle_type;
+			let TypeTuple { paren_token, elems } = typle_type;
 			let mut types: Punctuated<Type, Comma> = Punctuated::new();
 			for new_ty in elems.iter() {
 				let field = generate_one(new_ty.clone(), generics, pat_ident.clone(), vis.clone());
@@ -162,11 +190,11 @@ fn generate_one(ty: Type, generics: &mut Generics, pat_ident: PatIdent, vis: Vis
 
 			let field = generate_normal_field(pat_ident, vis, new_ty);
 			field
-		},
+		}
 		_ => {
 			let field = generate_normal_field(pat_ident, vis, ty);
 			field
-		},
+		}
 	}
 }
 
@@ -184,25 +212,17 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 
 	// check if input is valid
 	if let Some(constness) = input.sig.constness {
-		diagnostics.push(syn::Error::new_spanned(
-			constness,
-			"component function must not be const"
-		).into())
+		diagnostics
+			.push(syn::Error::new_spanned(constness, "component function must not be const").into())
 	} else if let Some(asyncness) = input.sig.asyncness {
-		diagnostics.push(syn::Error::new_spanned(
-			asyncness,
-			"component function must not be async",
-		).into());
+		diagnostics
+			.push(syn::Error::new_spanned(asyncness, "component function must not be async").into());
 	} else if let Some(unsafety) = input.sig.unsafety {
-		diagnostics.push(syn::Error::new_spanned(
-			unsafety,
-			"component function must not be unsafe",
-		).into());
+		diagnostics
+			.push(syn::Error::new_spanned(unsafety, "component function must not be unsafe").into());
 	} else if let Some(ref abi) = input.sig.abi {
-		diagnostics.push(syn::Error::new_spanned(
-			abi,
-			"component function must not have an abi",
-		).into());
+		diagnostics
+			.push(syn::Error::new_spanned(abi, "component function must not have an abi").into());
 	}
 
 	let mut generics = input.sig.generics.clone();
@@ -211,7 +231,13 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 
 	for arg in input.sig.inputs.iter() {
 		if let FnArg::Receiver(receiver) = arg {
-			diagnostics.push(syn::Error::new_spanned(receiver.self_token, "component function must not have self argument").into());
+			diagnostics.push(
+				syn::Error::new_spanned(
+					receiver.self_token,
+					"component function must not have self argument",
+				)
+				.into(),
+			);
 		} else if let FnArg::Typed(pat_type) = arg {
 			let pat = *pat_type.pat.clone();
 			let ty = *pat_type.ty.clone();
@@ -219,10 +245,10 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 				syn::Pat::Ident(pat_ident) => {
 					let field = generate_one(ty, &mut generics, pat_ident, attr.vis.clone());
 					fields.push(field);
-				},
+				}
 				_ => {
 					diagnostics.push(syn::Error::new_spanned(pat, "couldn't parse function argument").into());
-				},
+				}
 			}
 		}
 	}
@@ -230,7 +256,7 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 	let ident = attr.name.clone();
 	let vis = attr.vis.clone();
 
-	let generated_struct = quote!{
+	let generated_struct = quote! {
 		#[derive(::rstml_component::HtmlComponent)]
 		#vis struct #ident #generics {#fields}
 	};
@@ -244,7 +270,7 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 		fn_args.push(quote!(self.#ident,));
 	}
 
-	let impl_block = quote!{
+	let impl_block = quote! {
 		impl #impl_generics ::rstml_component::HtmlContent for #ident #ty_generics #where_clause {
 			fn fmt(self, formatter: &mut ::rstml_component::HtmlFormatter) -> std::fmt::Result {
 				formatter.write_content(#input_ident (#(#fn_args)*))
@@ -252,9 +278,7 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 		}
 	};
 
-	let diagnostics = diagnostics
-		.iter()
-		.map(|d| d.clone().emit_as_item_tokens());
+	let diagnostics = diagnostics.iter().map(|d| d.clone().emit_as_item_tokens());
 	quote! {
 		#(#diagnostics)*
 		#generated_struct
