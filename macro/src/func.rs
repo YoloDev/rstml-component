5f8788ddc7dc7177891032a1f118ac6bb343746d
davidon-top <davidkozakke@gmail.com>
2023-11-14 21:06:35 +0100

    fix: use builder pattern and cleanup code

--- macro/src/func.rs
+++ macro/src/func.rs
@@ -1,12 +1,10 @@
 use proc_macro2::{Span, TokenStream};
 use proc_macro2_diagnostics::{Diagnostic, Level};
-use quote::{quote, ToTokens};
+use quote::{format_ident, quote};
 use syn::{
-	parse::Parse,
-	punctuated::Punctuated,
-	token::{Brace, Bracket, Comma, Paren},
-	Field, FnArg, GenericParam, Generics, Ident, Pat, Token, Type, TypeImplTrait, TypeParam,
-	TypeTuple, Visibility, spanned::Spanned,
+	parse::Parse, punctuated::Punctuated, spanned::Spanned, token::Comma, Field, FnArg, GenericParam,
+	Generics, Ident, Pat, Path, PathArguments, PathSegment, Token, Type, TypeImplTrait, TypeParam,
+	TypePath, Visibility,
 };
 
 struct ComponentAttrs {
@@ -23,230 +21,154 @@ impl Parse for ComponentAttrs {
 	}
 }
 
-fn create_unique_generic_ident(
-	used: Punctuated<GenericParam, Comma>,
-	ident_suggestion: Option<Ident>,
-) -> Ident {
-	let mut ident = ident_suggestion.unwrap_or_else(|| Ident::new("V", Span::call_site()));
-	while used.iter().any(|used| {
-		if let GenericParam::Type(type_param) = used {
-			type_param.ident == ident
-		} else {
-			false
-		}
-	}) {
-		ident = Ident::new(&format!("{}T", ident), Span::call_site());
-	}
-	ident
-}
-
-fn lowercase_ident(ident: Ident) -> Ident {
+fn lowercase_ident(ident: &Ident) -> Ident {
 	Ident::new(&ident.to_string().to_lowercase(), ident.span())
 }
 
-fn is_field_ident_used(ident: &Ident, used: Vec<Pat>) -> bool {
-	used.iter().any(|used| match used.clone() {
-		Pat::Ident(ident_pat) => ident_pat.ident == *ident,
-		Pat::Slice(slice_pat) => {
-			let mut new_used = Vec::new();
-			new_used.extend(slice_pat.elems);
-			is_field_ident_used(ident, new_used)
-		}
-		Pat::TupleStruct(tuple_struct_pat) => {
-			let mut new_used = Vec::new();
-			new_used.extend(tuple_struct_pat.elems);
-			is_field_ident_used(ident, new_used)
-		}
-		Pat::Struct(struct_pat) => {
-			let mut new_used = Vec::new();
-			new_used.extend(struct_pat.fields.iter().map(|field| *field.pat.clone()));
-			is_field_ident_used(ident, new_used)
-		}
-		Pat::Tuple(tuple_pat) => {
-			let mut new_used = Vec::new();
-			new_used.extend(tuple_pat.elems);
-			is_field_ident_used(ident, new_used)
-		}
-		_ => false,
-	})
-}
-
-fn create_unique_field_ident(
-	used: Punctuated<Field, Comma>,
-	all_used: Punctuated<FnArg, Comma>,
-	ident_suggestion: Option<Ident>,
-) -> Ident {
-	let mut ident =
-		lowercase_ident(ident_suggestion.unwrap_or_else(|| Ident::new("arg", Span::call_site())));
-	while used.iter().any(|used| used.ident == Some(ident.clone())) {
-		ident = Ident::new(&format!("{}_", ident), Span::call_site());
-	}
-	let mut used = Vec::new();
-	used.extend(all_used.iter().map(|arg| {
-		if let FnArg::Typed(pat_type) = arg {
-			*pat_type.pat.clone()
-		} else {
-			unreachable!()
-		}
-	}));
-	while is_field_ident_used(&ident, used.clone()) {
-		ident = Ident::new(&format!("{}_", ident), Span::call_site());
-	}
-	ident
-}
-
-trait Surround: Clone {
-	fn trait_surround(self, tokens: TokenStream) -> TokenStream;
-}
-
-impl Surround for Brace {
-	fn trait_surround(self, tokens: TokenStream) -> TokenStream {
-		quote!({#tokens})
-	}
-}
-
-impl Surround for Bracket {
-	fn trait_surround(self, tokens: TokenStream) -> TokenStream {
-		quote!([#tokens])
-	}
-}
-
-impl Surround for Paren {
-	fn trait_surround(self, tokens: TokenStream) -> TokenStream {
-		quote!((#tokens))
-	}
-}
-
-fn prefix_type(ty: &mut Type, prefix: TokenStream) {
-	let type_tokens = ty.into_token_stream();
-	*ty = Type::Verbatim(quote!(#prefix #type_tokens))
-}
-
-fn suffix_type(ty: &mut Type, suffix: TokenStream) {
-	let type_tokens = ty.into_token_stream();
-	*ty = Type::Verbatim(quote!(#type_tokens #suffix))
-}
-
-fn surround_type(ty: &mut Type, surround: impl Surround) {
-	let type_tokens = ty.into_token_stream();
-	*ty = Type::Verbatim(surround.trait_surround(type_tokens))
-}
-
-fn generate_generic(
+struct ComponentStructBuilder {
+	vis: syn::Visibility,
+	ident: syn::Ident,
 	generics: Generics,
-	ident: Ident,
-	impl_type: TypeImplTrait,
-) -> (Field, TypeParam) {
-	let type_ident = create_unique_generic_ident(
-		generics.params,
-		Some(Ident::new(
-			ident.to_string().to_uppercase().as_str(),
-			Span::call_site(),
-		)),
-	);
-	let type_param = TypeParam {
-		attrs: vec![],
-		ident: type_ident.clone(),
-		colon_token: Some(Token![:](Span::call_site())),
-		bounds: impl_type.bounds,
-		eq_token: None,
-		default: None,
-	};
-	let field = Field {
-		attrs: vec![],
-		vis: Visibility::Public(Token![pub](Span::call_site())),
-		mutability: syn::FieldMutability::None,
-		ident: Some(ident),
-		ty: Type::Verbatim(type_ident.into_token_stream()),
-		colon_token: Some(Token![:](Span::call_site())),
-	};
-	(field, type_param)
+	fields: Punctuated<Field, Comma>,
 }
 
-fn generate_normal_field(ident: Ident, ty: Type) -> Field {
-	Field {
-		attrs: vec![],
-		vis: Visibility::Public(Token![pub](Span::call_site())),
-		mutability: syn::FieldMutability::None,
-		ident: Some(ident),
-		ty: Type::Verbatim(ty.into_token_stream()),
-		colon_token: Some(Token![:](Span::call_site())),
+impl ComponentStructBuilder {
+	fn new(attr: ComponentAttrs, generics: Generics) -> Self {
+		Self {
+			vis: attr.vis,
+			ident: attr.name,
+			generics,
+			fields: Punctuated::new(),
+		}
 	}
-}
 
-fn generate_one(ty: Type, generics: &mut Generics, ident: Ident) -> Field {
-	match ty {
-		Type::ImplTrait(impl_type) => {
-			let (field, type_param) = generate_generic(generics.clone(), ident, impl_type);
-			generics.params.push(GenericParam::Type(type_param));
-			field
-		}
-		Type::Array(array_type) => {
-			let new_ty = *array_type.elem;
-			let len = array_type.len;
-			let mut field = generate_one(new_ty, generics, ident);
-			suffix_type(&mut field.ty, quote!(; #len));
-			surround_type(&mut field.ty, array_type.bracket_token);
-			field
-		}
-		Type::Paren(paren_type) => {
-			let new_ty = *paren_type.elem;
-			let mut field = generate_one(new_ty, generics, ident);
-			surround_type(&mut field.ty, paren_type.paren_token);
-			field
-		}
-		Type::Ptr(ptr_type) => {
-			let new_ty = *ptr_type.elem;
-			let mut prefix_tokens = ptr_type.star_token.into_token_stream();
-			if let Some(mutability) = ptr_type.mutability {
-				prefix_tokens.extend(mutability.into_token_stream());
-			}
-			if let Some(const_token) = ptr_type.const_token {
-				prefix_tokens.extend(const_token.into_token_stream());
-			}
-			let mut field = generate_one(new_ty, generics, ident);
-			prefix_type(&mut field.ty, prefix_tokens);
-			field
-		}
-		Type::Reference(ref_type) => {
-			let new_ty = *ref_type.elem;
-			let mut prefix_tokens = ref_type.and_token.into_token_stream();
-			if let Some(lifetime) = ref_type.lifetime {
-				prefix_tokens.extend(lifetime.into_token_stream());
-			}
-			if let Some(mutability) = ref_type.mutability {
-				prefix_tokens.extend(mutability.into_token_stream());
-			}
-			let mut field = generate_one(new_ty, generics, ident);
-			prefix_type(&mut field.ty, prefix_tokens);
-			field
-		}
-		Type::Slice(slice_type) => {
-			let new_ty = *slice_type.elem;
-			let mut field = generate_one(new_ty, generics, ident);
-			surround_type(&mut field.ty, slice_type.bracket_token);
-			field
-		}
-		Type::Tuple(typle_type) => {
-			let TypeTuple { paren_token, elems } = typle_type;
-			let mut types: Punctuated<Type, Comma> = Punctuated::new();
-			for new_ty in elems.iter() {
-				let field = generate_one(new_ty.clone(), generics, ident.clone());
-				types.push(field.ty);
-			}
+	fn create_unique_field_ident(&self, ident: &Ident) -> Ident {
+		format_ident!("{}__{}", lowercase_ident(ident), self.fields.len())
+	}
 
-			let new_ty = Type::Tuple(TypeTuple {
-				paren_token,
-				elems: types,
-			});
+	fn create_unique_generic_ident(&self) -> Ident {
+		format_ident!("__T{}", self.generics.params.len())
+	}
 
-			let field = generate_normal_field(ident, new_ty);
-			field
+	fn resolve_type(&mut self, ty: &Type) -> Type {
+		match ty {
+			Type::ImplTrait(inner) => self.add_generic(inner.clone()),
+			Type::Array(inner) => {
+				let elem = self.resolve_type(&inner.elem);
+				Type::Array(syn::TypeArray {
+					bracket_token: inner.bracket_token,
+					elem: Box::new(elem),
+					semi_token: inner.semi_token,
+					len: inner.len.clone(),
+				})
+			}
+			Type::Paren(inner) => {
+				let elem = self.resolve_type(&inner.elem);
+				Type::Paren(syn::TypeParen {
+					paren_token: inner.paren_token,
+					elem: Box::new(elem),
+				})
+			}
+			Type::Ptr(inner) => {
+				let elem = self.resolve_type(&inner.elem);
+				Type::Ptr(syn::TypePtr {
+					star_token: inner.star_token,
+					const_token: inner.const_token,
+					mutability: inner.mutability,
+					elem: Box::new(elem),
+				})
+			}
+			Type::Reference(inner) => {
+				let elem = self.resolve_type(&inner.elem);
+				Type::Reference(syn::TypeReference {
+					and_token: inner.and_token,
+					lifetime: inner.lifetime.clone(),
+					mutability: inner.mutability,
+					elem: Box::new(elem),
+				})
+			}
+			Type::Slice(inner) => {
+				let elem = self.resolve_type(&inner.elem);
+				Type::Slice(syn::TypeSlice {
+					bracket_token: inner.bracket_token,
+					elem: Box::new(elem),
+				})
+			}
+			Type::Tuple(inner) => {
+				let elems = inner
+					.elems
+					.iter()
+					.map(|elem| self.resolve_type(elem))
+					.collect();
+				Type::Tuple(syn::TypeTuple {
+					paren_token: inner.paren_token,
+					elems,
+				})
+			}
+			_ => ty.clone(),
 		}
-		_ => {
-			let field = generate_normal_field(ident, ty);
-			field
-		}
+	}
+
+	fn push_field(&mut self, ident: Ident, ty: Type) {
+		let ty = self.resolve_type(&ty);
+
+		let field = Field {
+			attrs: vec![],
+			vis: Visibility::Public(Token![pub](Span::call_site())),
+			mutability: syn::FieldMutability::None,
+			ident: Some(ident),
+			ty,
+			colon_token: Some(Token![:](Span::call_site())),
+		};
+
+		self.fields.push(field);
+	}
+
+	fn push_non_unique_field(&mut self, ident: &Ident, ty: Type) {
+		let ident = self.create_unique_field_ident(ident);
+		self.push_field(ident, ty);
+	}
+
+	fn add_generic(&mut self, impl_type: TypeImplTrait) -> Type {
+		let type_ident = self.create_unique_generic_ident();
+		let type_param = TypeParam {
+			attrs: vec![],
+			ident: type_ident.clone(),
+			colon_token: Some(Token![:](Span::call_site())),
+			bounds: impl_type.bounds,
+			eq_token: None,
+			default: None,
+		};
+
+		self.generics.params.push(GenericParam::Type(type_param));
+
+		Type::Path(TypePath {
+			qself: None,
+			path: Path {
+				leading_colon: None,
+				segments: Punctuated::from_iter(vec![PathSegment {
+					ident: type_ident,
+					arguments: PathArguments::None,
+				}]),
+			},
+		})
+	}
+
+	fn build(self) -> (TokenStream, Ident, Generics, Punctuated<Field, Comma>) {
+		let Self {
+			vis,
+			ident,
+			generics,
+			fields,
+		} = self;
+		let (impl_generics, _, where_clause) = generics.split_for_impl();
+
+		let generated_struct = quote! {
+			#[derive(::rstml_component::HtmlComponent)]
+			#vis struct #ident #impl_generics #where_clause {#fields}
+		};
+
+		(generated_struct, ident, generics, fields)
 	}
 }
 
@@ -264,89 +186,81 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 
 	// check if input is valid
 	if let Some(constness) = input.sig.constness {
-		diagnostics
-			.push(Diagnostic::spanned(constness.span(), Level::Error, "component function must not be const"))
+		diagnostics.push(Diagnostic::spanned(
+			constness.span(),
+			Level::Error,
+			"component function must not be const",
+		))
 	} else if let Some(asyncness) = input.sig.asyncness {
-		diagnostics
-			.push(Diagnostic::spanned(asyncness.span(), Level::Error, "component function must not be async").into());
+		diagnostics.push(
+			Diagnostic::spanned(
+				asyncness.span(),
+				Level::Error,
+				"component function must not be async",
+			)
+			.into(),
+		);
 	} else if let Some(unsafety) = input.sig.unsafety {
-		diagnostics
-			.push(Diagnostic::spanned(unsafety.span(), Level::Error, "component function must not be unsafe").into());
+		diagnostics.push(
+			Diagnostic::spanned(
+				unsafety.span(),
+				Level::Error,
+				"component function must not be unsafe",
+			)
+			.into(),
+		);
 	} else if let Some(ref abi) = input.sig.abi {
-		diagnostics
-			.push(Diagnostic::spanned(abi.span(), Level::Error, "component function must not have an abi").into());
+		diagnostics.push(
+			Diagnostic::spanned(
+				abi.span(),
+				Level::Error,
+				"component function must not have an abi",
+			)
+			.into(),
+		);
 	}
 
-	let mut generics = input.sig.generics.clone();
-
-	let mut fields: Punctuated<Field, syn::token::Comma> = Punctuated::new();
+	let mut struct_builder = ComponentStructBuilder::new(attr, input.sig.generics.clone());
 
 	for arg in input.sig.inputs.iter() {
 		match arg {
 			FnArg::Receiver(_) => {
 				diagnostics.push(
-					Diagnostic::spanned(arg.span(), Level::Error, "component function must not have self argument").into(),
+					Diagnostic::spanned(
+						arg.span(),
+						Level::Error,
+						"component function must not have self argument",
+					)
+					.into(),
 				);
 			}
 			FnArg::Typed(pat_type) => {
 				let pat = *pat_type.pat.clone();
 				let ty = *pat_type.ty.clone();
 				match pat {
-					Pat::Ident(ident_pat) => {
-						let field = generate_one(ty, &mut generics, ident_pat.ident);
-						fields.push(field);
-					}
-					Pat::TupleStruct(tuple_struct_pat) => {
-						let ident = create_unique_field_ident(
-							fields.clone(),
-							input.sig.inputs.clone(),
-							// path segments should always have at least one element so unwrap is ok
-							Some(tuple_struct_pat.path.segments.last().unwrap().ident.clone()),
-						);
-						let field = generate_one(ty, &mut generics, ident);
-						fields.push(field);
-					}
-					Pat::Struct(struct_pat) => {
-						let ident = create_unique_field_ident(
-							fields.clone(),
-							input.sig.inputs.clone(),
-							// path segments should always have at least one element so unwrap is ok
-							Some(struct_pat.path.segments.last().unwrap().ident.clone()),
-						);
-						let field = generate_one(ty, &mut generics, ident);
-						fields.push(field);
-					}
+					Pat::Ident(ident_pat) => struct_builder.push_field(ident_pat.ident, ty),
+					Pat::TupleStruct(tuple_struct_pat) => struct_builder
+						.push_non_unique_field(&tuple_struct_pat.path.segments.last().unwrap().ident, ty),
+					Pat::Struct(struct_pat) => struct_builder
+						.push_non_unique_field(&struct_pat.path.segments.last().unwrap().ident, ty),
 					Pat::Tuple(_tuple_pat) => {
-						let ident = create_unique_field_ident(fields.clone(), input.sig.inputs.clone(), None);
-						let field = generate_one(ty, &mut generics, ident);
-						fields.push(field);
+						struct_builder.push_non_unique_field(&format_ident!("tuple"), ty)
 					}
 					Pat::Slice(_slice_pat) => {
-						let ident = create_unique_field_ident(fields.clone(), input.sig.inputs.clone(), None);
-						let field = generate_one(ty, &mut generics, ident);
-						fields.push(field);
+						struct_builder.push_non_unique_field(&format_ident!("slice"), ty)
 					}
 					_ => {
-						diagnostics
-							.push(Diagnostic::spanned(pat.span(), Level::Error, "couldn't parse function argument").into());
+						diagnostics.push(
+							Diagnostic::spanned(pat.span(), Level::Error, "couldn't parse function argument")
+								.into(),
+						);
 					}
 				}
 			}
 		}
 	}
 
-	let ident = attr.name;
-	let vis = match attr.vis {
-		syn::Visibility::Inherited => input.vis.clone(),
-		v => v,
-	};
-
-	let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
-
-	let generated_struct = quote! {
-		#[derive(::rstml_component::HtmlComponent)]
-		#vis struct #ident #impl_generics #where_clause {#fields}
-	};
+	let (generated_struct, ident, generics, fields) = struct_builder.build();
 
 	let input_ident = input.sig.ident.clone();
 	let mut fn_args = Vec::new();
@@ -355,6 +269,8 @@ pub fn component(attr: TokenStream, input: TokenStream) -> TokenStream {
 		let ident = field.ident.clone().unwrap();
 		fn_args.push(quote!(self.#ident,));
 	}
+
+	let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
 
 	let impl_block = quote! {
 		impl #impl_generics ::rstml_component::HtmlContent for #ident #ty_generics #where_clause {
